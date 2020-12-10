a75259de8ddd5fc7adec80d7c6463bc745f259c4
narison <sonche16@hotmail.com>
2020-12-10 17:52:21 +0100

    created laravel project

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'welcome to the ao-webshop';
+
+        return view('pages.index')->with([
+            'title'=>$title
+        ]);
+    }
+}
